7f21f2dcd90d21fa5741940497d4fb75bd58e992
srutownik <marek.28.93@gmail.com>
2014-07-12 01:56:34 +0200

    Poprawiono Gaussa; dodano obsługę multiselect

--- desktop/src/plugins/Convolution.java
+++ desktop/src/plugins/Convolution.java
@@ -40,7 +40,7 @@ public class Convolution extends MarvinAbstractImagePlugin {
 
     @Override
     public void process(MarvinImage mi, MarvinImage mi1, MarvinAttributes ma, MarvinImageMask mim, boolean bln) {
-        int[] input = mi.getIntColorArray(), output = mi.getIntColorArray();
+        int[] input = mi.getIntColorArray(), output = input.clone();
         int cWidth = mi.getWidth(), cHeight = mi.getHeight();
         double[] filter = (double[]) getAttribute("filter");
         Integer width = (Integer) getAttribute("width"),
@@ -50,23 +50,27 @@ public class Convolution extends MarvinAbstractImagePlugin {
         BoundGetter bg = new MirrorGetter(input, cWidth, mim);
         
         int xTranslation = width/2, yTranslation = height/2;
-        int gray;
         for (int y = 0; y < cHeight; y++) {
             for (int x = 0; x < cWidth; x++) {
                 if (mask != null && !mask[x][y]) continue;
 
                 int index = x + cWidth*y;
                 output[index] = 0;
+                int r = 0, g = 0, b = 0;
                 for (int fy = 0; fy < height; fy++) {
                     for (int fx = 0; fx < width; fx++) {
-                        gray = (int) (filter[fx + fy * width] * (0xFF & bg.get(
-                                                        x - xTranslation + fx, y - yTranslation + fy
-                                                )));
-                        output[index] += 0xFF000000 | gray<<16 | gray<<8 | gray;
+                        double f = filter[fx + fy * width];
+                        int c = bg.get(x - xTranslation + fx, y - yTranslation + fy);
+
+                        r += (int) (f * (0xFF0000 & c))>>16;
+                        g += (int) (f * (0xFF00 & c))>>8;
+                        b += (int) (f * (0xFF & c));
                     }
                 }
+                output[index] = 0xFF000000 | r<<16 | g<<8 | b;
             }
         }
+        mi1.setIntColorArray(output);
     }
     
     @Override
