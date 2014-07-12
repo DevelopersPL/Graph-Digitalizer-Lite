e559f2ab07d137f80773d5eb0c5d4c989935e37d
srutownik <marek.28.93@gmail.com>
2014-07-12 11:43:17 +0200

    Wybaczcie, nie dodałem tych plików

--- desktop/src/plugins/HistogramStretching.java
+++ desktop/src/plugins/HistogramStretching.java
@@ -29,8 +29,8 @@ public class HistogramStretching extends MarvinAbstractImagePlugin {
         return null;
     }
 
-    @Override
-    public void process(MarvinImage mi, MarvinImage mi1, MarvinAttributes ma, MarvinImageMask mim, boolean bln) {
+
+    public void proces(MarvinImage mi, MarvinImage mi1, MarvinAttributes ma, MarvinImageMask mim, boolean bln) {
         final int w = mi.getWidth();
         final int count = w * mi.getHeight();
         int[] h = new int[0x100];
@@ -43,7 +43,7 @@ public class HistogramStretching extends MarvinAbstractImagePlugin {
 
         for (int x = 0; x < mi.getWidth(); x++) {
             for (int y = 0; y < mi.getHeight(); y++) {
-                ++h[mi.getIntComponent2(x, y)>>16];
+                ++h[mi.getIntComponent0(x, y)];
             }
         }
 
@@ -61,17 +61,22 @@ public class HistogramStretching extends MarvinAbstractImagePlugin {
         }
         double minD = d[n];
 
+        int [] LUT = new int[256];
+        for (int i = 0; i < 256; i++) {
+            LUT[i] = (int) ((d[i]-minD)*255./(1-minD));
+        }
+
         int a, Y, Cr, Cb;
         for (int x = 0; x < mi.getWidth(); x++) {
             for (int y = 0; y < mi.getHeight(); y++) {
                 if (mask != null && !mask[x][y]) continue;
 
                 a = mi.getAlphaComponent(x, y);
-                Y = mi.getIntComponent0(x, y);
-                Cb= mi.getIntComponent1(x, y);
-                Cr= mi.getIntComponent2(x, y);
+                Y = YCrCb.getIntComponent0(x, y);
+                Cb= YCrCb.getIntComponent1(x, y);
+                Cr= YCrCb.getIntComponent2(x, y);
 
-                Y = (int) ((d[Y] - minD) * 0xFF / (1. - minD));
+                Y = LUT[Y];
 
                 r = (int) Math.abs(Y                        + 1.402   * (Cr - 128));
                 g = (int) Math.abs(Y - 0.34414 * (Cb - 128) - 0.71414 * (Cr - 128));
@@ -81,7 +86,67 @@ public class HistogramStretching extends MarvinAbstractImagePlugin {
             }
         }
     }
-    
+
+    public void process(MarvinImage mi, MarvinImage mi1, MarvinAttributes ma, MarvinImageMask mim, boolean bln) {
+        int r = mi.getWidth()*mi.getHeight();
+        int[] h = new int[256];
+        boolean[][] mask = mim.getMaskArray();
+
+        MarvinImagePlugin plugin = new RGB2YCbCr();
+        MarvinImage YCrCb = mi.clone();
+        plugin.process(mi, YCrCb, mim);
+
+        for (int x = 0; x < mi.getWidth(); x++) {
+            for (int y = 0; y < mi.getHeight(); y++) {
+                ++h[mi.getIntComponent2(x, y)];
+            }
+        }
+
+        double[] d = new double[256];
+        for (int i = 0; i < 256; ++i) {
+            if (i > 0) d[i] = d[i-1];
+            d[i] += h[i];
+            System.out.println(d[i] + " " + h[i]);
+        }
+        System.out.println(r);
+        for (int i = 0; i < 256; ++i) {
+            d[i] /= r;
+            System.out.println(d[i] + " " + h[i]);
+        }
+
+        int n = 0;
+        while (d[n] < 0)
+            ++n;
+
+        double minD = d[n];
+
+        int [] LUT = new int[256];
+        for (int i = 0; i < 256; i++) {
+            LUT[i] = (int) ((d[i]-minD)*255./(1-minD));
+        }
+
+        int a, Y, Cr, Cb, g, b;
+        for (int x = 0; x < mi.getWidth(); x++) {
+            for (int y = 0; y < mi.getHeight(); y++) {
+                if (mask != null && !mask[x][y]) continue;
+
+                a = mi.getAlphaComponent(x, y);
+                Y = YCrCb.getIntComponent0(x, y);
+                Cr= YCrCb.getIntComponent1(x, y);
+                Cb= YCrCb.getIntComponent2(x, y);
+
+                Y = LUT[Y];
+
+                r = (int) Math.abs(Y              + 1.403 * Cr);
+                g = (int) Math.abs(Y - 0.344 * Cb - 0.714 * Cr);
+                b = (int) Math.abs(Y + 1.77  * Cb);
+
+                mi1.setIntColor(x, y, a, r, g, b);
+            }
+        }
+    }
+
+
     @Override
     public String toString() {
         return "Wyrównanie histogramu";
